40ff79f83623c678ccf269741c0dfc368e066dd5
Zachary Coursey <zcoursey22@gmail.com>
2018-02-01 23:28:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return delete [playlist].[artistName];
+  return delete [playlist].artistName;
 }
