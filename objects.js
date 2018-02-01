61f0bf5b8b6766e011349ca7fbed99e802c46e51
Zachary Coursey <zcoursey22@gmail.com>
2018-02-01 23:14:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artist: 'song'};
 
 function updatePlaylist(obj, artist, song) {
-  return Object.assign({}, obj, {obj[artist]: song});
+  return Object.assign({}, obj, {artist: song});
 }
